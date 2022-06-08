f8bf3ad47d6ff8cd22abf226b6af3287070698a3
Guy Porat <guyporat7@gmail.com>
2022-06-08 06:11:13 +0300

    added option to detach trace from proc.

--- main_menu.c
+++ main_menu.c
@@ -12,12 +12,55 @@ void wait_for_key_press() {
 }
 
 
-void attach_to_process(){
-    size_t i, empty_cell_idx;
+int find_pid_cell_idx(int pid) {
+    size_t i;
+    for(i=0; (ATTACHED_PROCESSES[i] != pid) && (i < ARRAY_SIZE(ATTACHED_PROCESSES)); ++i);
+    if (i < ARRAY_SIZE(ATTACHED_PROCESSES)) {
+        return i;
+    }
+    return -1;
+}
+
+
+int get_pid_from_user(){
     int pid;
     printw("Please enter pid to attach to: ");
     while ((scanw("%d", &pid) != 1) && (MIN_PID <= pid && pid <= MAX_PID)) {
         printw("Bad input format. Please enter a valid pid: ");
+    }
+    return pid;
+}
+
+
+void detach_from_process(){
+    int pid, pid_cell_idx;
+    pid = get_pid_from_user();
+    pid_cell_idx = find_pid_cell_idx(pid);
+    if (pid_cell_idx == -1) {
+        printw("Process %d has no trace attached. (at least not one with this process as its tracer)\n", pid);
+        wait_for_key_press();
+        return;
+    }
+    // detach trace from process
+    if (ptrace(PTRACE_DETACH, pid, 0, 0) == -1) {
+        perror("Failed to detach from process");
+    } else {
+        ATTACHED_PROCESSES[pid_cell_idx] = EMPTY_PID_CELL_MARKER;
+        printw("Process trace detached.\n");
+    }
+    wait_for_key_press();    
+}
+
+
+void attach_to_process(){
+    size_t i, empty_cell_idx;
+    int pid;
+
+    pid = get_pid_from_user();
+    if (find_pid_cell_idx(pid) != -1) {
+        printw("Process %d was already attached a trace.", pid);
+        wait_for_key_press();
+        return;
     }
     for(i=0; (ATTACHED_PROCESSES[i] != EMPTY_PID_CELL_MARKER) && (i < ARRAY_SIZE(ATTACHED_PROCESSES)); ++i);
     if ( i >= ARRAY_SIZE(ATTACHED_PROCESSES)) {
@@ -26,12 +69,6 @@ void attach_to_process(){
         return;
     } 
     empty_cell_idx = i;
-    for(i=0; (ATTACHED_PROCESSES[i] != pid) && (i < ARRAY_SIZE(ATTACHED_PROCESSES)); ++i);
-    if (i < ARRAY_SIZE(ATTACHED_PROCESSES)) {
-        printw("Process %d was already attached a trace.", pid);
-        wait_for_key_press();
-        return;
-    }
     // attach trace to process
     if (ptrace(PTRACE_ATTACH, pid, 0, 0) == -1) {
         perror("Failed to attach to process");
--- main_menu.h
+++ main_menu.h
@@ -8,16 +8,56 @@
 
 extern int ATTACHED_PROCESSES[256];
 
+/**
+ * @brief Print the debugger's main menu and handle user input.
+ * 
+ */
 void main_menu();
+
+
+/**
+ * @brief Prompt user for pid and attach a trace to the specified process.
+ * 
+ */
 void attach_to_process();
+
+/**
+ * @brief Prompt user for pid and detach the trace from specified process.
+ * 
+ */
+void detach_from_process();
+
+/**
+ * @brief Prompt user to press any key and block untill he does so.
+ * 
+ */
+void wait_for_key_press();
+
+
+/**
+ * @brief Prompt user for pid and return the pid entered.
+ * 
+ * @return int 
+ */
+int get_pid_from_user();
+
+
+/**
+ * @brief Get the index of the pid in ATTACHED_PROCESSES array.\
+ *        Return -1 if pid not in ATTACHED_PROCESSES array.
+ * 
+ * @param pid 
+ * @return size_t 
+ */
+int find_pid_cell_idx(int pid);
 
 #define MAIN_MENU_CHOICES_LEN 4
 #define EMPTY_PID_CELL_MARKER 0
 #define MIN_PID	1
 #define MAX_PID	99999
 #define MAIN_MENU_CHOICES ((MenuChoice[])  {	\
-	{"attach", "attach to process", &attach_to_process},	\
-	{"dettach", "dettach from process", (void(*)()) NULL},	\
+	{"attach", "attach trace to process", &attach_to_process},	\
+	{"detach", "detach trace from process", &detach_from_process},	\
 	{"debug", "debug attached process", &debug_process_menu}	\
 })
 
