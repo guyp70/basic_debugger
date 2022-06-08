bbea21a865c5b47875c901dd9a5ab6b1e1275f64
Guy Porat <guyporat7@gmail.com>
2022-06-08 12:48:11 +0300

    Added option to resume attached process run.

--- process_debug.c
+++ process_debug.c
@@ -2,6 +2,18 @@
 
 
 void debug_process_menu(void *data) {
-    UNUSED(data);
-    make_menu(PROCESS_DEBUG_MENU_CHOICES, PROCESS_DEBUG_MENU_CHOICES_LEN);
+    int pid = *((int*) data);
+    
+    make_menu(PROCESS_DEBUG_MENU_CHOICES(pid), PROCESS_DEBUG_MENU_CHOICES_LEN);
+}
+
+
+void process_continue(void *data){
+    int pid = *((int*) data);
+    if (ptrace(PTRACE_CONT, pid, 0, 0) == -1) {
+        perror("Failed to continue process run.");
+    } else {
+        printw("Process run resumed.\n");
+    }
+    wait_for_key_press();
 }
--- process_debug.h
+++ process_debug.h
@@ -1,21 +1,29 @@
 #ifndef PROCESS_DEBUG_H
 #define PROCESS_DEBUG_H
 
+#include <sys/ptrace.h>
 #include "make_menu.h"
 
 
 /**
  * @brief Enter process debug menu and offer debuging operations on the specified process.
- *        Data should be a void pointer to pid int. (i.e. int pid = 1928; debug_process_menu((void*)pid);)
  * 
- * @param data 
+ * @param data Should be a void pointer to pid int. (i.e. int pid = 1928; debug_process_menu((void*)pid); )
  */
 void debug_process_menu(void *data);
 
+/**
+ * @brief Resume the attached process run. 
+ * 
+ * @param data Should be a void pointer to pid int. (i.e. int pid = 1928; debug_process_menu((void*)pid); )
+ */
+void process_continue(void *data);
 
-#define PROCESS_DEBUG_MENU_CHOICES_LEN    2
-#define PROCESS_DEBUG_MENU_CHOICES  ((MenuChoice[])  {    \
-    {"continue", "attach to process", (void(*)(void*)) NULL, (void*) NULL}   \
+
+#define PROCESS_DEBUG_MENU_CHOICES_LEN    1
+#define ARRAY_SIZE(a) (size_t)(sizeof(a) / sizeof(*a))
+#define PROCESS_DEBUG_MENU_CHOICES(pid)  ((MenuChoice[])  {    \
+    {"continue", "resume process run", process_continue, (void*) (&pid)}   \
 })
 #endif // PROCESS_DEBUG_H
 
